efddcc15183963a065f6755859c8bce883b5bfde
Reuben Frankel <rfrankel@matatika.com>
2025-08-12 14:26:05 +0100

    Change default start date back to 90 days ago

--- tap_googleads/tap.py
+++ tap_googleads/tap.py
@@ -85,7 +85,7 @@ class TapGoogleAds(Tap):
         secret=True,
     )
     _end_date = datetime.now(timezone.utc).date()
-    _start_date = _end_date - timedelta(days=7)
+    _start_date = _end_date - timedelta(days=90)
 
     # TODO: Add Descriptions
     config_jsonschema = th.PropertiesList(
