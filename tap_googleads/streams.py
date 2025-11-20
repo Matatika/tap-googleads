3b15868ccaf38084f99ce06fe89a786763a0cc1d
Reuben Frankel <rfrankel@matatika.com>
2025-11-20 02:19:34 +0000

    Keep using paginated search method in the customer hierarchy stream for compatibility with current stream class relationships

--- tap_googleads/streams.py
+++ tap_googleads/streams.py
@@ -64,6 +64,7 @@ class CustomerHierarchyStream(GoogleAdsStream):
     know when to query the down stream apps
     """
 
+    path = "/customers/{customer_id}/googleAds:search"
     records_jsonpath = "$.results[*]"
     name = "customer_hierarchy"
     primary_keys = ["customerClient__id"]
@@ -146,4 +147,6 @@ class CustomerHierarchyStream(GoogleAdsStream):
 
 class ReportsStream(GoogleAdsStream):
     """Base class for all report streams."""
+
     parent_stream_type = CustomerHierarchyStream
+    path = "/customers/{customer_id}/googleAds:searchStream"
