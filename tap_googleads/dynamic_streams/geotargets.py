f6d183495a98f89a112a4dcf1f5c4e6434d4b8ef
Mathew Abraham <mabraham@matatika.com>
2026-03-31 11:56:09 +0100

    Removed extraction of parent_geo_target_id which will now be handled in stream maps or in dbt

--- tap_googleads/dynamic_streams/geotargets.py
+++ tap_googleads/dynamic_streams/geotargets.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING, Any, Dict, Iterable
 
 from tap_googleads.dynamic_query_stream import DynamicQueryStream
@@ -29,22 +28,6 @@ class GeotargetsStream(DynamicQueryStream):
     name = "geo_target_constant"
     primary_keys = ["geoTargetConstant__id"]
 
-    @cached_property
-    def schema(self) -> dict:
-        schema = super().schema
-        schema["properties"]["parent_geo_target_id"] = {"type": ["integer", "null"]}
-        return schema
-
-    def post_process(self, row, context=None) -> dict | None:
-        flattened_row = super().post_process(row, context)
-
-        parent_geo_target = flattened_row.get("geoTargetConstant__parentGeoTarget")
-        if isinstance(parent_geo_target, str) and "/" in parent_geo_target:
-            flattened_row["parent_geo_target_id"] = int(parent_geo_target.rsplit("/", 1)[-1])
-        else:
-            flattened_row["parent_geo_target_id"] = None
-
-        return flattened_row
 
     def get_records(self, context: Context) -> Iterable[Dict[str, Any]]:
         """Return a generator of record-type dictionary objects.
